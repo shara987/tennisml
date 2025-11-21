6460b4251e1cc50d8d76697155bec09399a12e9f
Sharanya Singh <149442287+shara987@users.noreply.github.com>
2025-11-20 20:31:01 -0500

    Add Encoder and Decoder classes for autoencoder
    Implemented Encoder and Decoder classes for a neural network autoencoder using PyTorch.

--- train_model.py
+++ train_model.py
@@ -3,24 +3,34 @@ import json
 import os
 from sklearn.model_selection import train_test_split
 #should use sklearn.preprocessing....
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import torch.nn as nn
+import torch.nn.functional as F
 
-def load_dataset(data_dir="training_data"):
-    return 0
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1) # Input channels, output channels
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
 
-def create_autoencoder_model(input_shape):
-    return 0
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.convT1 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+        self.convT2 = nn.ConvTranspose2d(16, 3, kernel_size=2, stride=2)
+        self.sigmoid = nn.Sigmoid() # For image data scaled between 0 and 1
 
-def train_autoencoder(args):
-    return 0
+    def forward(self, x):
+        x = self.relu(self.convT1(x))
+        x = self.sigmoid(self.convT2(x))
+        return x
 
-#decide which classifier/model, lstm?
-def create_classifier_model(args):
-    return 0
-
-def train_classifier(args):
-    return 0
 
 #save model, maybe using pickle??

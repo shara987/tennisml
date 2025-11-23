892195f945ba9199ce5e770c211c309089655769
ducanh19082007 <bendy.anhle@gmail.com>
2025-11-23 14:03:24 -0500

    minor changes

---
+++ preprocessing/frame_extration.py
@@ -0,0 +1,11 @@
+from rembg import remove
+from PIL import Image
+import os
+
+"""
+    you should download thes libraries
+    
+    pip install pillow
+    pip install rembg
+    pip install onnxruntime
+"""
